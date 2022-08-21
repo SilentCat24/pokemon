a56196d7afa4eb39d00c7b6b1b75b97afab87bcc
Arun R Kumar <arunmahi921@gmail.com>
2022-08-21 22:33:40 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
-import "./App.css";
+
 import axios from "axios";
 import Pagination from "./Pagination";
 
